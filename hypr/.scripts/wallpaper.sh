f1e3efdb227b0b5a90f2475375b4e2cb040cc7ca
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2024-06-01 01:49:58 +0300

    Create wallpaper.sh

---
+++ hypr/.scripts/wallpaper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+directory=~/Изображения/wallpaper
+monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
+
+if [ -d "$directory" ]; then
+    random_background=$(ls $directory/* | shuf -n 1)
+
+    hyprctl hyprpaper unload all
+    hyprctl hyprpaper preload $random_background
+    hyprctl hyprpaper wallpaper "$monitor, $random_background"
+fi
