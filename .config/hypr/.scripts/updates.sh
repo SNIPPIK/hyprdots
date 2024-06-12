23eda2e8e69643eca0a473b3f9819c471ae40d0f
SNIPPIK <55327334+SNIPPIK@users.noreply.github.com>
2024-06-12 20:51:24 +0300

    Update updates.sh

--- .config/hypr/.scripts/updates.sh
+++ .config/hypr/.scripts/updates.sh
@@ -8,7 +8,7 @@
 #
 # by Stephan Raabe (2023)
 # -----------------------------------------------------
-# Requires pacman-contrib trizen
+# Requires pacman-contrib
 
 # -----------------------------------------------------
 # Define threshholds for color indicators
@@ -19,29 +19,14 @@ threshhold_yellow=25
 threshhold_red=100
 
 # -----------------------------------------------------
-# Calculate available updates pacman and aur (with trizen)
+# Calculate available updates pacman
 # -----------------------------------------------------
 
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-    updates_aur=0
-fi
-
-updates=$(("$updates_arch" + "$updates_aur"))
-
-# -----------------------------------------------------
-# Testing
-# -----------------------------------------------------
-
-# Overwrite updates with numbers for testing
-# updates=100
-
-# test JSON output
-# printf '{"text": "0", "alt": "0", "tooltip": "0 Updates", "class": "red"}'
-# exit
+updates=$(("$updates_arch"))
 
 # -----------------------------------------------------
 # Output in JSON format for Waybar Module custom-updates
