6dbb7842598565a0eadf29dfc1f00ec8deac39f0
snippik <xdox.xdox@yandex.ru>
2024-07-09 19:13:00 +0300

    Updated screenshot script

---
+++ .config/hypr/scripts/screenshot.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# -----------------------------------------------------
+directory="${HOME}/Pictures/Screenshots"
+file="$(date "+%H:%M:%S %d.%m.%Y").png"
+# -----------------------------------------------------
+# Create directory
+if [ ! -d "$directory" ]; then
+  mkdir "$directory"
+fi
+# -----------------------------------------------------
+# Create screenshot region
+if [ "$1" = "region" ]; then
+  grim -g "$(slurp)" "$directory/$file"
+fi
+# -----------------------------------------------------
+# Create screenshot fullscreen
+if [ "$1" = "fullscreen" ]; then
+  grim "$directory/$file"
+fi
