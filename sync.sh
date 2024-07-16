011adf8ab7ee078749ca9cecd748900a4c65bd00
snippik <xdox.xdox@yandex.ru>
2024-07-16 22:37:53 +0300

    Fixed bash sync

--- sync.sh
+++ sync.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 # Open hyprdots
-cd ~/hyprdots
+cd ~/
 
 # Update hyprdots
 if [ -d ~/hyprdots ]; then
-  git reset --hard origin
+  rm -rd ~/hyprdots
+
+  sleep 1
+  git clone https://github.com/SNIPPIK/hyprdots.git
+
+  sleep 1
+  cd ~/hyprdots
+  bash ./install.sh
 else
   echo "Fail update, need to install! Not found ~/hyprdots!"
 fi
-
-# Reinstall
-bash ./install.sh
