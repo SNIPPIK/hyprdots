f299af2358213d1aa715c0663bf54549d121270e
snippik <xdox.xdox@yandex.ru>
2024-07-18 22:11:15 +0300

    Added echo

--- sync.sh
+++ sync.sh
@@ -5,18 +5,23 @@ cd ~/
 
 # Update hyprdots
 if [ -d ~/hyprdots ]; then
+  echo Remove old hyprdots
   sudo rm -rd ~/hyprdots
 
+  echo Install new hyprdots
   sleep 1
   git clone https://github.com/SNIPPIK/hyprdots.git
 
+  echo Run installer
   sleep 1
   cd ~/hyprdots
   bash ./install.sh
 
   sleep 1
-  # Restart waybar
-  bash ~/.config/hypr/scripts/utils/reload.sh waybar
+  echo Pls restart a waybar. WIN + F11
+
+  sleep 5
+  exit 0
 else
   echo "Fail update, need to install! Not found ~/hyprdots!"
 fi
