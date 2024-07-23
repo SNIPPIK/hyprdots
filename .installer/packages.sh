2f2d33be00c39d8f7585fea7afa572bb71644090
snippik <xdox.xdox@yandex.ru>
2024-07-23 18:38:37 +0300

    Ops, fix installer

--- .installer/packages.sh
+++ .installer/packages.sh
@@ -1,12 +1,12 @@
 # Install arch packages
 echo Install need packages...
-#sudo pacman -S hyprland hyprpaper hypridle hyprlock waybar otf-font-awesome noto-fonts-emoji rofi-wayland nm-connection-editor pipewire-pulse wireplumber bluez blueberry pavucontrol nautilus polkit-gnome swaync grim slurp pacman-contrib sddm fastfetch starship gnome-keyring ttf-nerd-fonts-symbols-mono ttf-dejavu yad
+sudo pacman -S hyprland hyprpaper hypridle hyprlock waybar otf-font-awesome noto-fonts-emoji rofi-wayland nm-connection-editor pipewire-pulse wireplumber bluez blueberry pavucontrol nautilus polkit-gnome swaync grim slurp pacman-contrib sddm fastfetch starship gnome-keyring ttf-nerd-fonts-symbols-mono ttf-dejavu
 
 sleep 1
 
 # Run systemctl services
 echo Run sddm service and bluetooth service
-#sudo systemctl enable sddm && sudo systemctl enable bluetooth
+sudo systemctl enable sddm && sudo systemctl enable bluetooth
 
 sleep 1
 
