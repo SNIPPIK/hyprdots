42e7c87c6ffc33f95482189aa4b94de141310137
snippik <xdox.xdox@yandex.ru>
2024-07-23 11:58:57 +0300

    Added hyprlock in hyprland-git

--- .installer/hyprland.sh
+++ .installer/hyprland.sh
@@ -2,16 +2,16 @@
 # Remove current libs and install git libs
 if [ "$1" == "git" ]; then
   echo Remove current packages...
-  sudo pacman -Rsc hyprland hyprutils hyprcursor hyprlang xdg-desktop-portal-wlr
+  sudo pacman -Rsc hyprland hyprutils hyprcursor hyprlang hyprlock xdg-desktop-portal-wlr
 
   sleep 0.5
 
   if yay
   then
-      yay -S hyprland-git hyprlang-git hyprcursor-git hyprutils-git
+      yay -S hyprland-git hyprlang-git hyprcursor-git hyprutils-git hyprlock-git
   elif paru
   then
-      paru -S hyprland-git hyprlang-git hyprcursor-git hyprutils-git
+      paru -S hyprland-git hyprlang-git hyprcursor-git hyprutils-git hyprlock-git
   else
       echo Not found aur installers, pls install yay or paru!
       exit 1
@@ -24,8 +24,8 @@ fi
 # Remove git libs and install stable libs
 if [ "$1" == "stable" ]; then
   echo Remove git packages...
-  sudo pacman -Rsc hyprland-git hyprutils-git hyprcursor-git hyprlang-git
+  sudo pacman -Rsc hyprland-git hyprutils-git hyprcursor-git hyprlang-git hyprlock-git
 
   sleep 0.5
-  sudo pacman -S hyprland hyprutils hyprcursor hyprlang hyprpaper hypridle libdisplay-info xdg-desktop-portal-hyprland
+  sudo pacman -S hyprland hyprutils hyprcursor hyprlang hyprpaper hypridle hyprlock libdisplay-info xdg-desktop-portal-hyprland
 fi
