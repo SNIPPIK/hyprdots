7caf9a259a3077077a72e468db0eacba4b84aefa
snippik <xdox.xdox@yandex.ru>
2024-08-01 22:29:35 +0300

    Stop! again?! WTF! Fix in dialog.sh

--- Files/Configs/hypr/scripts/dialog.sh
+++ Files/Configs/hypr/scripts/dialog.sh
@@ -3,5 +3,5 @@
 if [ "$1" = "float" ]; then
   alacritty --class hyprdots-floating -e bash ~/.config/hypr/scripts/dialogs/"$2".sh
 else
-  ~/.config/hypr/scripts/dialogs/"$1".sh
+  bash ~/.config/hypr/scripts/dialogs/"$1".sh
 fi
