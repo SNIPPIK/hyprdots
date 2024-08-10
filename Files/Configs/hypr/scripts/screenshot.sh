3706446915bf07b2bb8d09c26622c344124ccdd6
snippik <xdox.xdox@yandex.ru>
2024-08-11 00:09:24 +0300

    Update to 0.5.0

--- Files/Configs/hypr/scripts/screenshot.sh
+++ Files/Configs/hypr/scripts/screenshot.sh
@@ -11,9 +11,11 @@ fi
 # Create screenshot region
 if [ "$1" = "region" ]; then
   grim -g "$(slurp)" "$directory/$file"
+  wl-copy < "$directory/$file"
 fi
 # -----------------------------------------------------
 # Create screenshot fullscreen
 if [ "$1" = "fullscreen" ]; then
   grim "$directory/$file"
+  wl-copy < "$directory/$file"
 fi
