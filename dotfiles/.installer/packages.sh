ed58c4862be2b3513729c2234682fc80a3bba140
snippik <xdox.xdox@yandex.ru>
2024-08-21 17:46:21 +0300

    Remove nautilus, added nemo file manager

--- dotfiles/.installer/packages.sh
+++ dotfiles/.installer/packages.sh
@@ -1,6 +1,6 @@
 # Install arch packages
 echo Installing need packages...
-sudo pacman -S hyprland hyprpaper hypridle hyprlock waybar otf-font-awesome noto-fonts-emoji rofi-wayland nm-connection-editor pipewire-pulse wireplumber bluez blueberry pavucontrol nautilus polkit-gnome swaync grim slurp pacman-contrib sddm fastfetch starship gnome-keyring ttf-nerd-fonts-symbols-mono ttf-dejavu wl-clipboard
+sudo pacman -S hyprland hyprpaper hypridle hyprlock waybar otf-font-awesome noto-fonts-emoji rofi-wayland nm-connection-editor pipewire-pulse wireplumber bluez blueberry pavucontrol nemo polkit-gnome swaync grim slurp pacman-contrib sddm fastfetch starship gnome-keyring ttf-nerd-fonts-symbols-mono ttf-dejavu wl-clipboard
 
 sleep 1
 
