0b0ec6363129ca6fc01e548e2e505f08c8fb2d80
snippik <xdox.xdox@yandex.ru>
2024-08-22 22:35:46 +0300

    Change color logo and replace name in screenshots notification

--- dotfiles/Files/Configs/hypr/scripts/screenshot.sh
+++ dotfiles/Files/Configs/hypr/scripts/screenshot.sh
@@ -17,12 +17,12 @@ fi
 if [ "$1" = "region" ]; then
   grim -g "$(slurp)" "$directory/$file"
   wl-copy < "$directory/$file"
-  notify "Save in\n- Pictures/Screenshots/$file" "$directory/$file"
+  notify "Save in - Pictures/Screenshots/$file" "$directory/$file"
 fi
 # -----------------------------------------------------
 # Create screenshot fullscreen
 if [ "$1" = "fullscreen" ]; then
   grim "$directory/$file"
   wl-copy < "$directory/$file"
-  notify "Save in\n- Pictures/Screenshots/$file" "$directory/$file"
+  notify "Save in - Pictures/Screenshots/$file" "$directory/$file"
 fi
