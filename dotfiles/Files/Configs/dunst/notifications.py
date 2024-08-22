5073950d4fb009ba8de974ffde02a4ee5ddfc7a8
snippik <xdox.xdox@yandex.ru>
2024-08-22 19:51:18 +0300

    Small fixes and added new binds

--- dotfiles/Files/Configs/dunst/notifications.py
+++ dotfiles/Files/Configs/dunst/notifications.py
@@ -14,10 +14,10 @@ def format_history(history):
     alt = 'none'
     tooltip_click = []
     tooltip_click.append("󰎟 Notifications")
-    tooltip_click.append("󰳽 scroll-down:  history pop")
-    tooltip_click.append("󰳽 click-left:  Enable & Disable DND")
-    tooltip_click.append("󰳽 click-middle: 󰛌 clear history")
-    tooltip_click.append("󰳽 click-right: 󱄊 close all")
+    #tooltip_click.append("󰳽 scroll-down:   history pop")
+    tooltip_click.append("󰳽 click-left:    history pop")
+    tooltip_click.append("󰳽 click-middle: 󰛌 Clear history")
+    tooltip_click.append("󰳽 click-right:  󱄊 Close all")
 
     tooltip = []
 
