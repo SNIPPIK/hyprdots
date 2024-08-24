46331b90bb1028da0c4a316095b84c4fd3d9b883
snippik <xdox.xdox@yandex.ru>
2024-08-24 14:28:33 +0300

    Fixed a bug with a non-existent screenshot

--- dotfiles/Files/Configs/hypr/scripts/tools/screenshot.sh
+++ dotfiles/Files/Configs/hypr/scripts/tools/screenshot.sh
@@ -7,6 +7,17 @@ file="$(date "+%H:%M:%S %d.%m.%Y").png"
 notify() {
   bash ~/.config/hypr/scripts/utils/notifications.sh "icon" "temp" "Screenshot" "$1" "$2" 4000
 }
+
+# Check file
+find_screenshot() {
+  if [ ! -f "$directory/$file" ]; then
+    exit 0
+  fi
+
+  wl-copy < "$directory/$file"
+  notify "Save in - Pictures/Screenshots/$file" "$directory/$file"
+}
+
 # -----------------------------------------------------
 # Create directory
 if [ ! -d "$directory" ]; then
@@ -16,13 +27,11 @@ fi
 # Create screenshot region
 if [ "$1" = "region" ]; then
   grim -g "$(slurp)" "$directory/$file"
-  wl-copy < "$directory/$file"
-  notify "Save in - Pictures/Screenshots/$file" "$directory/$file"
+  find_screenshot
 fi
 # -----------------------------------------------------
 # Create screenshot fullscreen
 if [ "$1" = "fullscreen" ]; then
   grim "$directory/$file"
-  wl-copy < "$directory/$file"
-  notify "Save in - Pictures/Screenshots/$file" "$directory/$file"
+  find_screenshot
 fi
