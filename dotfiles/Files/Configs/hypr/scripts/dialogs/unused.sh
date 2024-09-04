5eab779f710bef35a39d12f39bc813dd0a1da79d
snippik <xdox.xdox@yandex.ru>
2024-09-04 20:37:14 +0300

    hyprodots - Added removing cache pacman and yay

--- dotfiles/Files/Configs/hypr/scripts/dialogs/unused.sh
+++ dotfiles/Files/Configs/hypr/scripts/dialogs/unused.sh
@@ -2,15 +2,23 @@
 if [ -z "$(sudo pacman -Qdtq)" ]; then
     echo "Not find packages for remove"
 else
-    echo "Remove unused packages"
+    echo "Removing unused packages"
     sudo pacman -Qdtq | sudo pacman -Rsc -
 fi
 
 sleep 0.2s
+
+# Cache removing
 clear
+echo "Removing cache in package manager"
+sudo pacman -Scc
+yay -Scc
+
+sleep 0.2s
 
 # Flatpak packages
-echo "Remove unused flatpak"
+clear
+echo "Removing unused flatpak application"
 flatpak uninstall --unused
 
 # Press enter
