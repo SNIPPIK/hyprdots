92edbabf187583da6bec80647d7ca56e6da7259b
snippik <xdox.xdox@yandex.ru>
2024-10-21 17:57:41 +0300

    Updated update and unused scripts

--- dotfiles/Files/Configs/hypr/scripts/dialogs/unused.sh
+++ dotfiles/Files/Configs/hypr/scripts/dialogs/unused.sh
@@ -1,23 +1,37 @@
 # Arch packages
+echo "     _             _     "
+echo "    / \   _ __ ___| |__  "
+echo "   / _ \ | '__/ __| '_ \ "
+echo "  / ___ \| | | (__| | | |"
+echo " /_/   \_\_|  \___|_| |_|"
 if [ -z "$(sudo pacman -Qdtq)" ]; then
     echo "Not find packages for remove"
 else
     echo "Removing unused packages"
     sudo pacman -Qdtq | sudo pacman -Rsc -
 fi
-
+sudo pacman -Scc
 sleep 0.2s
 
-# Cache removing
+# Aur cache removing
 clear
-echo "Removing cache in package manager"
-sudo pacman -Scc
+echo "     _               "
+echo "    / \  _   _ _ __  "
+echo "   / _ \| | | | '__| "
+echo "  / ___ \ |_| | |    "
+echo " /_/   \_\__,_|_|    "
+echo "Removing aur cache in package manager"
 yay -Scc
-
 sleep 0.2s
 
 # Flatpak packages
 clear
+echo "  _____ _       _               _     "
+echo " |  ___| | __ _| |_ _ __   __ _| | __ "
+echo " | |_  | |/ _' | __| '_ \ / _' | |/ / "
+echo " |  _| | | (_| | |_| |_) | (_| |   <  "
+echo " |_|   |_|\__,_|\__| .__/ \__,_|_|\_\ "
+echo "                   |_|                "
 echo "Removing unused flatpak application"
 flatpak uninstall --unused
 
--- dotfiles/Files/Configs/hypr/scripts/dialogs/update.sh
+++ dotfiles/Files/Configs/hypr/scripts/dialogs/update.sh
@@ -1,27 +1,45 @@
 # Update system packages
+echo "     _             _     "
+echo "    / \   _ __ ___| |__  "
+echo "   / _ \ | '__/ __| '_ \ "
+echo "  / ___ \| | | (__| | | |"
+echo " /_/   \_\_|  \___|_| |_|"
 echo "Updating the system and synchronizing with arch repositories"
 sudo pacman -Syyu
-
 sleep 0.2s
 
 # Update aur packages
 clear
+echo "     _               "
+echo "    / \  _   _ _ __  "
+echo "   / _ \| | | | '__| "
+echo "  / ___ \ |_| | |    "
+echo " /_/   \_\__,_|_|    "
 echo "Updating aur repository"
 
-# Install yay
-if ! yay; then
-  cd ~/.cache/
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si
-  sleep 0.2s
+# Install yay package
+if ! command -v yay &> /dev/null
+then
+    echo "Install yay"
+    cd ~/.cache/
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    sleep 0.2s
+    clear
 fi
-
+# Update aur packages
 yay -Syu
 sleep 0.2s
 
 # Update flatpak packages
 clear
+echo "  _____ _       _               _     "
+echo " |  ___| | __ _| |_ _ __   __ _| | __ "
+echo " | |_  | |/ _' | __| '_ \ / _' | |/ / "
+echo " |  _| | | (_| | |_| |_) | (_| |   <  "
+echo " |_|   |_|\__,_|\__| .__/ \__,_|_|\_\ "
+echo "                   |_|                "
 echo "Updating flatpak"
 flatpak update
 
