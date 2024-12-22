44f0a7e399ccf1f7a46bb556931df60427fdd435
snippik <xdox.xdox@yandex.ru>
2024-12-22 19:19:55 +0300

    DotFiles - Remove language switcher notification, postponed for revision

--- dotfiles/Files/Configs/hypr/scripts/utils/keyboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-notification_timeout=1000
-icon=~/.config/hypr/icons/translate.png
-
-# Switch language
-if [ $1 == "show" ]; then
-  lang=$(hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .active_keymap')
-  notify-send -t $notification_timeout -h string:category:volume "Language" "$lang language" -i "$icon"
-fi
